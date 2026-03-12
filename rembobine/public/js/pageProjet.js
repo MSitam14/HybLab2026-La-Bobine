2b891dde9e7b4af7ff380b9a73285dfdeba8d6be
Ryan <ryan.lamrous@etu.univ-nantes.fr>
2026-03-12 10:52:56 +0100

    Ajout de texte de base

--- rembobine/public/js/pageProjet.js
+++ rembobine/public/js/pageProjet.js
@@ -1,6 +1,10 @@
 "use strict";
 
+let Institutionnel = null;
+
+
 function createButtonBox(boxId = "box1") {
+ 
   const box = document.createElement("div");
   box.id = boxId;
   box.className = "box";
@@ -13,6 +17,8 @@ function createButtonBox(boxId = "box1") {
       const buttonNumber = rowIndex * 2 + columnIndex + 1;
       const button = document.createElement("button");
       button.id = `button${buttonNumber}`;
+      console.log(typeof(button.id));
+      console.log(button.id);
       button.textContent = ` ${buttonNumber} `;
       row.appendChild(button);
     }
@@ -22,8 +28,8 @@ function createButtonBox(boxId = "box1") {
     box.querySelectorAll('button').forEach(option => {
       option.addEventListener('click', async () => {
         // Send the user's choice to our API
-
-        const value = option.textContent;
+        console.log(option.textContent);
+        // const value = option.textContent;
 
         const box = option.parentElement.parentElement;
         // Remove all buttons
@@ -34,10 +40,12 @@ function createButtonBox(boxId = "box1") {
 
         // Change box background color
         box.style.backgroundColor = '#ff2ba3d1'; // Change color as desired
-
+        console.log(option.id);
         // Display clicked button text
         const textDisplay = document.createElement('p');
-        textDisplay.textContent = value;
+        console.log(Institutionnel[option.id])
+        textDisplay.textContent = Institutionnel[parseInt(option.textContent,10) - 1].Base;
+        console.log(textDisplay.textContent);
         box.appendChild(textDisplay);
 
         box.parentElement.appendChild(createButtonBox(`box`));
@@ -55,6 +63,13 @@ const initPageProjet = async function () {
   // Retrieve the partner's topic from our API
   let response = await fetch('api/topic');
   const data1 = await response.json();
+
+  const response2 = (await fetch('/rembobine/data/article.json'));
+  const article = await response2.json()
+  Institutionnel = article.Institutionnel;
+  console.log(article);
+  console.log(Institutionnel);
+
 
   const titre = document.getElementById('titre');
   titre.textContent = `Our topic is "${data1.topic}".`;
